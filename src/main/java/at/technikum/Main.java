08e15f277730bbe26557d517a62e7895404d1f46
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-01 17:51:57 +0100

    New Function inc controller. New HttpStatus. Exceptions now catched in own Controller

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -7,7 +7,7 @@ import at.technikum.server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(new TaskApp());
+        Server server = new Server(new MtcgApp());
         server.start();
     }
 }
--- src/main/java/at/technikum/apps/mtcg/controller/Controller.java
+++ src/main/java/at/technikum/apps/mtcg/controller/Controller.java
@@ -1,11 +1,39 @@
 package at.technikum.apps.mtcg.controller;
 
+import at.technikum.server.http.HttpContentType;
+import at.technikum.server.http.HttpStatus;
 import at.technikum.server.http.Request;
 import at.technikum.server.http.Response;
 
-public interface Controller {
+public abstract class Controller {
 
-    boolean supports(String route);
+    public abstract boolean supports(String route);
 
-    Response handle(Request request);
+    public abstract Response handle(Request request);
+
+    /**
+     * Return a Response using a httpStatus Code.
+     * The User will get the default error message of the status code as response.
+     */
+    protected Response status(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(HttpContentType.APPLICATION_JSON);
+        response.setBody("{ \"error\": \""+ httpStatus.getMessage() + "\"}");
+
+        return response;
+    }
+
+    /**
+     * Return a Response using a httpStatus Code.
+     * You can specify the error message of the status code the user receives
+     */
+    protected Response status(HttpStatus httpStatus, String errorMessage) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(HttpContentType.APPLICATION_JSON);
+        response.setBody("{ \"error\": \"" + errorMessage + "\"}");
+
+        return response;
+    }
 }
