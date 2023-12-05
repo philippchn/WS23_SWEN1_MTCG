a9401046ee30837d0e20c9df68ac0b9d002133a1
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-05 12:18:27 +0100

    401 und 409 hinzugefügt

--- src/main/java/at/technikum/server/http/HttpStatus.java
+++ src/main/java/at/technikum/server/http/HttpStatus.java
@@ -1,12 +1,15 @@
 package at.technikum.server.http;
 
 // THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
-public enum HttpStatus {
+public enum HttpStatus
+{
     OK(200, "OK"),
     CREATED(201, "CREATED"),
     BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
     NOT_FOUND(404, "Not Found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    CONFLICT(409, "Conflict"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;
