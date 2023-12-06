f467613964bfe5131e29eab4c0b4650a96f27c73
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-06 20:38:41 +0100

    Sessions path finished

--- src/main/java/at/technikum/apps/mtcg/controller/SessionController.java
+++ src/main/java/at/technikum/apps/mtcg/controller/SessionController.java
@@ -1,23 +1,37 @@
 package at.technikum.apps.mtcg.controller;
 
+import at.technikum.apps.mtcg.entity.Token;
+import at.technikum.apps.mtcg.entity.User;
+import at.technikum.apps.mtcg.service.SessionService;
 import at.technikum.server.http.HttpContentType;
 import at.technikum.server.http.HttpStatus;
 import at.technikum.server.http.Request;
 import at.technikum.server.http.Response;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class SessionController extends Controller {
+
+    private final SessionService sessionService = new SessionService();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public boolean supports(String route) {
         return route.startsWith("/sessions");
     }
 
     @Override
-    public Response handle(Request request) {
+    public Response handle(Request request)
+    {
         Response response = new Response();
 
         if (request.getRoute().equals("/sessions")) {
             return switch (request.getMethod()) {
-                case "POST" ->  status(HttpStatus.OK);
+                case "POST" ->  getToken(request);
                 default ->
                         status(HttpStatus.METHOD_NOT_ALLOWED);
             };
@@ -26,8 +40,47 @@ public class SessionController extends Controller {
         return response;
     }
 
-    void mapToTokenRequest(String username, String password)
-    {
-
+    private User jsonToUserRequest(Request request) throws JsonProcessingException {
+        return objectMapper.readValue(request.getBody(), User.class);
     }
+
+    private Response getToken(Request request)
+    {
+        User user;
+        try
+        {
+            user = jsonToUserRequest(request);
+        }
+        catch (JsonProcessingException e)
+        {
+            return statusCustomBody(HttpStatus.BAD_REQUEST, "JSON invalid");
+        }
+
+        Optional<Token> token;
+        try
+        {
+            token = sessionService.getToken(user);
+        }
+        catch (SQLException e)
+        {
+            return statusCustomBody(HttpStatus.NOT_FOUND, "User not found");
+        }
+
+        if (token.isEmpty())
+        {
+            return statusCustomBody(HttpStatus.UNAUTHORIZED, "Invalid username/password provided");
+        }
+
+        String tokenJson;
+        try
+        {
+            tokenJson = objectMapper.writeValueAsString(token.get());
+        }
+        catch (JsonProcessingException e)
+        {
+            return status(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return statusCustomBody(HttpStatus.OK, tokenJson);
+    }
+
 }
