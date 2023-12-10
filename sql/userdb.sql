d3b0d3938978a6c37d179db5e5c257957dfc5a64
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-10 12:14:09 +0100

    usertable updated and drawio added

--- sql/userdb.sql
+++ sql/userdb.sql
@@ -4,8 +4,12 @@ CREATE DATABASE userdb;
 
 CREATE TABLE IF NOT EXISTS usertable (
     username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    coins INT DEFAULT 20
 );
+
+ALTER TABLE usertable ADD COLUMN coins INT DEFAULT 20;
+
 
 CREATE TABLE IF NOT EXISTS userdatatable (
     username VARCHAR(255) PRIMARY KEY REFERENCES usertable(username),
