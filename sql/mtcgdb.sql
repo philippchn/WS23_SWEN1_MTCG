8673a5cff5ec30ac56d51834d7cc061a8be90e10
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-11 12:13:41 +0100

    card, package and usertopackage tables added

--- sql/mtcgdb.sql
+++ sql/mtcgdb.sql
@@ -14,3 +14,25 @@ CREATE TABLE IF NOT EXISTS t_userdata (
     bio VARCHAR(255) NOT NULL,
     image VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS t_card(
+    cardId VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    damage FLOAT,
+    monsterType BOOLEAN NOT NULL,
+    elementType VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS t_package (
+    packageId SERIAL PRIMARY KEY,
+    cardId_1 VARCHAR(255) REFERENCES t_card(cardId),
+    cardId_2 VARCHAR(255) REFERENCES t_card(cardId),
+    cardId_3 VARCHAR(255) REFERENCES t_card(cardId),
+    cardId_4 VARCHAR(255) REFERENCES t_card(cardId),
+    cardId_5 VARCHAR(255) REFERENCES t_card(cardId)
+);
+
+CREATE TABLE IF NOT EXISTS t_userToPackage (
+    username VARCHAR(255) REFERENCES t_user(username),
+    packageId INT REFERENCES t_package(packageId)
+);
