c86ccd7ce9d8337a06bf84263f5bc3090ef32b9e
Philipp Chn <19249044+philippchn@users.noreply.github.com>
2023-12-14 22:05:45 +0100

    sql updated to not allow null for all values

--- sql/mtcgdb.sql
+++ sql/mtcgdb.sql
@@ -17,22 +17,22 @@ CREATE TABLE IF NOT EXISTS t_userdata (
 
 CREATE TABLE IF NOT EXISTS t_card(
     cardId VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    damage FLOAT,
+    name VARCHAR(255) NOT NULL,
+    damage FLOAT NOT NULL,
     monsterType BOOLEAN NOT NULL,
-    elementType VARCHAR(255)
+    elementType VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS t_package (
     packageId SERIAL PRIMARY KEY,
-    cardId_1 VARCHAR(255) REFERENCES t_card(cardId),
-    cardId_2 VARCHAR(255) REFERENCES t_card(cardId),
-    cardId_3 VARCHAR(255) REFERENCES t_card(cardId),
-    cardId_4 VARCHAR(255) REFERENCES t_card(cardId),
-    cardId_5 VARCHAR(255) REFERENCES t_card(cardId)
+    cardId_1 VARCHAR(255) REFERENCES t_card(cardId) NOT NULL,
+    cardId_2 VARCHAR(255) REFERENCES t_card(cardId) NOT NULL,
+    cardId_3 VARCHAR(255) REFERENCES t_card(cardId) NOT NULL,
+    cardId_4 VARCHAR(255) REFERENCES t_card(cardId) NOT NULL,
+    cardId_5 VARCHAR(255) REFERENCES t_card(cardId) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS t_userToPackage (
-    username VARCHAR(255) REFERENCES t_user(username),
-    packageId INT REFERENCES t_package(packageId)
+    username VARCHAR(255) REFERENCES t_user(username) NOT NULL,
+    packageId INT REFERENCES t_package(packageId) NOT NULL
 );
